ecfaaed385b48453298e52030e1bb4ef5a173af1
purdygeoff <purdygeoff@hotmail.com>
2020-10-25 14:54:09 -0400

    Add project files.

---
+++ Demo/Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    public class Todo
+    {
+        public string MyText { get; set; }
+        public bool DDone { get; set; }
+    }
+}
